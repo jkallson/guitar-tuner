0a9bb87ebb83a046cb7c5d90c52d2339e4cfcf6a
jannoja <jannojl@gmail.com>
2018-11-11 13:41:33 +0200

    Sain pildi ja üritasin sinna nuppe peale panna, ei saanud hakkama

--- projekt1.py
+++ projekt1.py
@@ -1,7 +1,21 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+from PIL import Image, ImageTk
 
+
+raam = Tk()
+raam.title("Kitarrihäälestaja")
+raam.geometry("300x300")
+
+
+canv = Canvas(raam, height=300, width=300, bg ="white")
+canv.grid(row=0, column=0)
+
+img = ImageTk.PhotoImage(Image.open("pilt.jpg"))
+canv.create_image(20, 20, anchor=NW, image=img)
+canv.pack()
+mainloop()
 
 def tervita():
     tervitus = '0 väravat '
@@ -13,12 +27,24 @@ def ok():
     uus.geometry('300x300')
     
     if variable.get() == 'one':
+        E = ttk.Button(uus, text="Mängi", command=tervita)
+        E.place(x=0, y=0, width=100)
+
+        A = ttk.Button(uus, text="Mängi", command=tervita)
+        A.place(x=0, y=30, width=100)
+
+        B = ttk.Button(uus, text="Mängi", command=tervita)
+        B.place(x=0, y=60, width=100)
+
+        C = ttk.Button(uus, text="Mängi", command=tervita)
+        C.place(x=0, y=90, width=100)
+
         D = ttk.Button(uus, text="Mängi", command=tervita)
         D.place(x=0, y=120, width=100)
 
-raam = Tk()
-raam.title("Kitarrihäälestaja")
-raam.geometry("300x300")
+        F = ttk.Button(uus, text="Mängi", command=tervita)
+        F.place(x=0, y=150, width=100)
+        
 
 variable = StringVar(raam)
 variable.set("one")
@@ -30,23 +56,6 @@ button = Button(raam, text="OK", command=ok)
 button.pack()
 
 
-E = ttk.Button(raam, text="Mängi", command=tervita)
-E.place(x=0, y=0, width=100)
-
-A = ttk.Button(raam, text="Mängi", command=tervita)
-A.place(x=0, y=30, width=100)
-
-B = ttk.Button(raam, text="Mängi", command=tervita)
-B.place(x=0, y=60, width=100)
-
-C = ttk.Button(raam, text="Mängi", command=tervita)
-C.place(x=0, y=90, width=100)
-
-D = ttk.Button(raam, text="Mängi", command=tervita)
-D.place(x=0, y=120, width=100)
-
-F = ttk.Button(raam, text="Mängi", command=tervita)
-F.place(x=0, y=150, width=100)
 
 
 raam.mainloop()
