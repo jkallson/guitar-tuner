d369c65a62d1187db291bd6cee2f6634491d250c
jkallson <janar.kallson@gmail.com>
2018-11-15 10:22:10 +0200

    no message

--- kodu1.py
+++
@@ -1,8 +0,0 @@
-def seosta_lapsed_ja_vanemad(a,b):
-    f = open(a).read().split("\n")
-    vanemad = open(b).read().split("\n")
-    for element in f:
-        for i in vanemad:
-            print("sfas")
-
-seosta_lapsed_ja_vanemad("lapsed.txt","nimed.txt")
