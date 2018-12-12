7a73412fd9f83053d249ccc017a54364fc0abae7
jannoja <jannojl@gmail.com>
2018-12-12 15:37:19 +0200

    Laulud, akordid, varia

--- projekt.py
+++ projekt.py
@@ -3,24 +3,16 @@ from tkinter import ttk
 from tkinter import messagebox
 from PIL import Image, ImageTk
 from winsound import *
+from random import randint 
 
 raam = Tk()
 raam.title("Kitarrihäälestaja")
 raam.geometry("300x300")
-##raam.config(bg='black')
 
 
 background_image = ImageTk.PhotoImage(Image.open("pilt.jpg"))
 background_label = Label(raam, image=background_image)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-##canv = Canvas(raam, height=300, width=300, bg ="white")
-##canv.grid(row=0, column=0)
-##
-##img = ImageTk.PhotoImage(Image.open("pilt.jpg"))
-##canv.create_image(20, 20, anchor=NW, image=img)
-##canv.pack()
-##mainloop()
 
 a_keel = lambda: PlaySound('standard_a.wav', SND_FILENAME)
 e_korge = lambda: PlaySound('standard_e_korge.wav', SND_FILENAME)
@@ -35,10 +27,10 @@ drop_c_d = lambda: PlaySound('drop_c_d', SND_FILENAME)
 drop_c_f = lambda: PlaySound('drop_c_f', SND_FILENAME)
 drop_c_eelviimane = lambda: PlaySound('drop_c_eelviimane', SND_FILENAME)
 drop_c_viimane = lambda: PlaySound('drop_c_viimane', SND_FILENAME)
+laul1 = lambda: PlaySound("Stairway", SND_FILENAME)
+laul2 = lambda: PlaySound("Nothing", SND_FILENAME)
+laul3 = lambda: PlaySound("Laporte", SND_FILENAME)
 
-def tervita():
-    tervitus = '0 väravat '
-    messagebox.showinfo(message=tervitus) ##kast
 
 def ok():
     uus = Toplevel()
@@ -122,7 +114,52 @@ def answer():
     raam.destroy()
 Button(text='Quit', command=answer).pack(side=BOTTOM)
 
+def varia():
+    laused = ["Esimene kitarr loodi Vana-Egiptuses", "Maailma suurim kitarr on 13 meetrit pikk", "Maailma väikseim kitarr on 10 mikromeetrit pikk","Maailma kalleim kitarr maksab 2,8 miljonit dollarit","Inglismaal abiellus üks muusik oma kitarriga","Kitarrifirma Ibanez tõi esimest korda turule 7 ja 8 keelelised kitarrid", "Kõige pikem järjestikune kitarrimängimine kestis 114 tundi","Esimese elektrikitarri tegi Gibson 1936 aastal","Jimi Hendrix mängis kitarri tagurpidi, sest ta oli vasakukäeline","Fenderi kitarritööstus teeb päevas 90 000 keelt"]
+    suvaline = randint(1,10)
+    
+    for arv in range(1,11):
+        if arv == suvaline:
+            messagebox.showinfo('Varia',laused[arv-1])
+            
+        
+    
+varia = ttk.Button(raam, text="Varia", command=varia)
+varia.place(x=100, y=230, width=100)
 
+def akordid():
+    uus = Toplevel()
+    uus.title("Akordid")
+    uus.geometry('1000x321')
+    background_image = ImageTk.PhotoImage(Image.open("akordid.jpg"))
+    background_label = Label(uus, image=background_image)
+    background_label.image = background_image
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+    
+akordid = ttk.Button(raam, text="Akordid", command=akordid)
+akordid.place(x=100, y=200, width=100)
+
+def muusika():
+    uus = Toplevel()
+    uus.title(variable.get())
+    uus.geometry('300x300')
+    background_image = ImageTk.PhotoImage(Image.open("pilt.jpg"))
+    background_label = Label(uus, image=background_image)
+    background_label.image = background_image
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+    
+    Stairway = ttk.Button(uus, text="Stairway to heaven", command=laul1)
+    Stairway.place(x=0, y=0, width=200)
+
+    Nothing = ttk.Button(uus, text="Nothing else matters", command=laul2)
+    Nothing.place(x=0, y=30, width=200)
+
+    Laporte = ttk.Button(uus, text="Black widow of la porte", command=laul3)
+    Laporte.place(x=0, y=60, width=200)
+    
+    
+laulud = ttk.Button(raam, text="♪♫♬", command=muusika)
+laulud.place(x=270, y=275, width=30)
 
 raam.mainloop()
 
